ced8e36eca90c428b71a0d18d488d29195a650af
jvdon <joaovictordonato2@gmail.com>
2022-10-10 00:56:21 -0300

    Change Formating

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const PORT = 8000;
 const app = express();
 const db = new sqlite.Database(__dirname + "/user");
 
-let produtos = ['Cartão de Credito','Cartão de Débito','Previdência privada','Cheques','Título de Capitalização','Consórcio','Financiamento','Empréstimos','Seguro de Vida']
+let produtos = ['Cartão de Credito', 'Cartão de Débito', 'Previdência privada', 'Cheques', 'Título de Capitalização', 'Consórcio', 'Financiamento', 'Empréstimos', 'Seguro de Vida']
 
 app.set('view engine', 'ejs');
 
@@ -16,29 +16,28 @@ app.use(parser.urlencoded());
 
 app.get("/", (req, res) => {
       res.render("pages/index");
-})
+});
 
 app.get("/busca", (req, res) => {
       res.render("pages/busca");
-})
+});
 
 app.get("/cadastro", (req, res) => {
-      res.render("pages/cadastro", {produtos});
-})
+      res.render("pages/cadastro", { produtos });
+});
 
 app.get("/user", (req, res) => {
       let id = req.query["id"];
-
       let sql = `SELECT * FROM user WHERE id = '${id}';`;
+
       db.get(sql, (err, row) => {
             if (err) {
                   res.redirect("/");
             } else {
                   res.render("pages/user", { user: row });
             }
-      })
-
-})
+      });
+});
 
 app.get("/listar", (req, res) => {
       db.all("SELECT * FROM user;", (err, rows) => {
@@ -48,18 +47,19 @@ app.get("/listar", (req, res) => {
                   res.render("pages/listar", { result: rows });
             }
       });
-})
+});
 
 app.get("/buscaCPF", (req, res) => {
       let cpf = req.query["cpf"];
       let sql = `SELECT * FROM user WHERE CPF LIKE '%${cpf}%';`;
+
       db.all(sql, (err, row) => {
             if (err || row.length == 0) {
                   res.render("pages/busca", { result: "Usuário não encontrado!", url: cpf });
             } else {
                   res.render("pages/busca", { result: row, url: cpf });
             }
-      })
+      });
 });
 
 
@@ -73,8 +73,8 @@ app.get("/editar", (req, res) => {
             } else {
                   res.render("pages/editar", { user: row, produtos: produtos });
             }
-      })
-})
+      });
+});
 
 app.get("/remover", (req, res) => {
       let id = req.query["id"];
@@ -84,7 +84,7 @@ app.get("/remover", (req, res) => {
       db.exec(sql, (err) => {
             res.redirect("/");
       });
-})
+});
 
 app.post("/cadastro", (req, res) => {
       let { nome, cpf, genero, telefone, email, tipoConta, endereco, dataNascimento, produto } = req.body;
@@ -96,8 +96,6 @@ app.post("/cadastro", (req, res) => {
       let dataCadastro = (new Date()).toLocaleDateString().replace(/\-/g, "/");
       dataNascimento = (new Date(dataNascimento)).toLocaleDateString().replace(/\-/g, "/");
 
-      console.log(produto);
-
       let sql = `INSERT INTO user (profUrl, nome, cpf, idade, genero, tel, email, tipoConta, end, dataNascimento, dataCadastro, produtos) VALUES ('${profUrl}','${nome}', '${cpf}', ${idade}, '${genero}', '${telefone}', '${email}' ,'${tipoConta}', '${endereco}', '${dataNascimento}', '${dataCadastro}', '${JSON.stringify(produto)}');`
       db.exec(sql, (err) => {
             if (err) {
@@ -107,8 +105,8 @@ app.post("/cadastro", (req, res) => {
             } else {
                   res.redirect("/");
             }
-      })
-})
+      });
+});
 
 app.post("/editar", (req, res) => {
       let id = req.query["id"];
@@ -124,15 +122,14 @@ app.post("/editar", (req, res) => {
       let sql = `UPDATE user SET profUrl = '${profUrl}', nome = '${nome}', cpf = '${cpf}', idade = ${idade}, genero = '${genero}', tel = '${telefone}', email = '${email}', tipoConta = '${tipoConta}', end = '${endereco}', dataNascimento = '${dataNascimento}', dataCadastro = '${dataCadastro}', produtos = '${JSON.stringify(produto)}' WHERE id = ${id}`;
       db.exec(sql, (err) => {
             if (err) {
-                  res.render("pages/editar", {user: {}, err: "Erro ao editar usuário: " + err.message });
+                  res.render("pages/editar", { user: {}, err: "Erro ao editar usuário: " + err.message });
             } else {
                   res.redirect("/listar");
             }
-      })
-})
+      });
+});
 
 app.listen(PORT, () => { console.log(`Listening on http://localhost:${PORT}`) });
-
 
 function getAge(dateString) {
       var today = new Date();
